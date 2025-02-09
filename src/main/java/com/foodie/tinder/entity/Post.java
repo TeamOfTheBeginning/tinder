812c98d0a8d25c617aef89d44cb452ddcc302129
caskerhello <caskerhello@naver.com>
2025-02-09 23:20:55 +0900

    fix : 빌드 그래들 수정 feat : 엔티티조금(DB작성중)

---
+++ src/main/java/com/foodie/tinder/entity/Post.java
@@ -0,0 +1,25 @@
+package com.foodie.tinder.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+
+@Entity
+@Data
+public class Post {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "post_id")
+    private int postId;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "member_id")
+    Member member;
+
+    private String content;
+
+    @CreationTimestamp
+    private Timestamp writedate;
+}
