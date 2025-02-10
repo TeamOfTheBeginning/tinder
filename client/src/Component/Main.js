335597f2c9aff9c736988c4572da7445921307a6
naegangheo <hng8907@gmail.com>
2025-02-10 09:57:15 +0900

    feat : main 도입

---
+++ client/src/Component/Main.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react'
+import { useNavigate } from "react-router-dom";
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+
+import MainMenu from './MainMenu'
+import Post from './post/Post';
+import '../style/Posts.css';
+
+const Main = () => {
+
+    const [postList, setPostList ] = useState([]);
+    const navigate = useNavigate();
+    const [followings, setFollowings]=useState([]);
+    const [paging, setPaging] = useState({})
+    const [word, setWord] = useState('n')
+    const loginUser = useSelector(state=>state.user);
+
+    useEffect(
+        ()=>{
+            setFollowings( [...loginUser.followings] )
+
+            axios.get(`/api/post/getPostList`, {params:{word}})
+            .then((result)=>{
+                setPostList( result.data.postList );
+            }).catch((err)=>{console.error(err)})
+
+        }, [word]
+    )
+    return (
+        <div style={{display:"flex", flexDirection:"column", alignItems:"center"}} >
+            <MainMenu setWord={setWord} />
+            <div className='Posts'>
+                {
+                    (postList)?(
+                        postList.map((post, idx)=>{
+                            return (
+                                <Post key={idx} post={post}  followings={followings}  setFollowings={setFollowings} />
+                            )
+                        })
+                    ):(null)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Main
