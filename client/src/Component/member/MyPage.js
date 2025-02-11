6295c5842a86d697a67bf5c44855947488b43279
leeSeungGyu <lsg12795@naver.com>
2025-02-11 11:32:47 +0900

    feat : 로그인 redux

---
+++ client/src/Component/member/MyPage.js
@@ -0,0 +1,91 @@
+import React, {useState, useEffect} from 'react'
+import '../../style/mypage.css'
+import axios from 'axios';
+import { useNavigate } from "react-router-dom";
+import MainMenu from '../MainMenu';
+import { useSelector } from 'react-redux';
+
+const MyPage = () => {
+
+    const loginUser = useSelector( state=>state.user );
+    const [ imgSrc, setImgSrc ]=useState('');
+    const navigate=useNavigate();
+    const [word, setWord] = useState('n')
+    const [imgList, setImgList] = useState([])
+    
+    useEffect(
+        ()=>{
+            if( loginUser.profileimg ){
+                console.log(loginUser.profileimg);
+                setImgSrc(`http://localhost:8070/userimg/${loginUser.profileimg}`);
+            }
+            axios.get('/api/member/getMyPost')
+            .then((result)=>{
+                setImgList( [...result.data.imgList] );
+            }).catch((err)=>{console.error(err)})
+
+        },[]
+    )
+
+    return (
+        <div className='mypage'>
+            <MainMenu  setWord={setWord}/>
+            <div className='userinfo'>
+                <div className='img'>
+                    <img src={imgSrc} />
+                </div>
+                <div className='profile'>
+                    <div className='field'>
+                        <label>E-mail</label>
+                        <div>{loginUser.email}</div>
+                    </div>
+                    <div className='field'>
+                        <label>Nick Name</label>
+                        <div>{loginUser.nickname}</div>
+                    </div>
+                    {/* <div className='field'>
+                        <label>Followers</label>
+                        <div>
+                            {
+                                (loginUser.followers)?(loginUser.followers.length):(0)
+                            }
+                        </div>
+                    </div>
+                    <div className='field'>
+                        <label>Followings</label>
+                        <div>
+                            {
+                                (loginUser.followings)?(loginUser.followings.length):(0)
+                            }
+                        </div>
+                    </div> */}
+                    <div className='field'>
+                        <label>intro</label>
+                        <div>{loginUser.profilemsg}</div>
+                    </div>
+                </div>
+            </div>
+
+            <div className='btns' >
+                <div id ="btn" onClick={()=>{ navigate('/editProfile')}}>Edit Profile</div>
+                <div id ="btn">Post Write</div>
+            </div>
+
+            <div className='userpost' >
+                {
+                    (imgList)?(
+                        imgList.map((imgs, idx)=>{
+                            return (
+                                <div key={idx}>
+                                    <img src={`http://localhost:8070/images/${imgs}`} />
+                                </div>
+                            )
+                        })
+                    ):(<div>Loading...</div>)
+                }
+            </div>
+        </div>
+    )
+}
+
+export default MyPage
