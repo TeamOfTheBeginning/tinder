1a05bfd9a693b106b9e9d14fc68970acbcbc04d3
soo <noowoo90@gmail.com>
2025-03-06 15:10:52 +0900

    style : Login - 카카오 로그인 페이지 CSS 수정 및 회원가입 페이지 검증 추가 수정

--- client/src/Component/member/Savekakaoinfo.js
+++ client/src/Component/member/Savekakaoinfo.js
@@ -3,18 +3,18 @@ import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
 import DaumPostcode from 'react-daum-postcode';
 import * as PortOne from "@portone/browser-sdk/v2";
-import '../../style/member/savekakaoinfo.css';
 import { IoCreateOutline } from "react-icons/io5";
 import AddressModal from './AddressModal';
 
+import '../../style/member/savekakaoinfo.css';
+import '../../style/login.css';
 import Loading from '../Loading';
 
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, useLocation } from 'react-router-dom';
 
 import { useDispatch } from 'react-redux';
 import { loginAction, setFollower, setFollowed } from '../../store/userSlice';
-import {Cookies, useCookies } from 'react-cookie'
-import { useLocation } from 'react-router-dom';
+import {Cookies, useCookies } from 'react-cookie';
 
 import jaxios from '../../util/jwtUtil';
 import { setCookie1, getCookie1 } from '../../util/cookieUtil2';
@@ -184,15 +184,19 @@ const Savekakaoinfo = () => {
         console.log(longitude)
         console.log(latitude)
         console.log(address)
-        if(!adultVerification){ return alert('성인인증을 해주세요')}
-        if(email===''){ return alert('이메일을 입력하세요');}
-        if(nickname==''){ return alert('닉네임을 입력하세요');}
-        if(zipnum==''||address==''){ return alert('주소 검색을 하세요');}
-        if(profileimg==''){ return alert('프로필 이미지를 업로드 하세요');}
+        if(!adultVerification){ return alertAndScroll('성인인증을 해주세요', 'adultVerification');}
+        if(email===''){ return alertAndScroll('이메일을 입력하세요', 'email');}
+        if(nickname===''){ return alertAndScroll('닉네임을 입력하세요', 'nickname');}
+        if(zipnum===''||address===''){ return alertAndScroll('주소 검색을 하세요', 'zipnum');}
+        if (intro.length > 255) {
+            alertAndScroll('자기소개는 255자 이하로 작성해주세요.', 'intro');
+            return;
+        }
+        if(profileimg===''){ return alertAndScroll('프로필 이미지를 업로드 하세요', 'profileimg');}
         
         try{            
             let result = await axios.post('/api/member/nicknamecheck', null, {params:{nickname}} );
-            if(result.data.msg == 'no' ){
+            if(result.data.msg === 'no' ){
                 return alert('닉네임이 중복됩니다');
             }
             console.log({
@@ -242,6 +246,25 @@ const Savekakaoinfo = () => {
         }catch(err){  console.error(err);     }
     }
 
+    const handleIntroChange = (e) => {
+        let value = e.target.value;
+        if (value.length > 255) {
+            alertAndScroll('자기소개는 255자 이하로 작성해주세요.(공백 포함', 'intro');
+            value = value.slice(0, 255);
+        }
+        setIntro(value);
+    };
+
+    // 검증 오류 alert 후 해당 입력창으로 스크롤 이동하는 함수
+    const alertAndScroll = (message, fieldId) => {
+        alert(message);
+        const element = document.getElementById(fieldId);
+        if (element) {
+            element.scrollIntoView({ behavior: 'smooth', block: 'center' });
+            element.focus();
+        }
+    };
+
     const handleLoadingComplete = () => {
         navigate('/main'); // 메인 페이지로 이동
         setLoadingComplete(true);
@@ -250,17 +273,22 @@ const Savekakaoinfo = () => {
     const [isLoginSuccess, setIsLoginSuccess] = useState(false);
     const [loadingComplete, setLoadingComplete] = useState(false);
 
-    async function fileUpload(e){
-        const formData = new FormData();
-        formData.append('image',  e.target.files[0]);
-        const result = await axios.post('/api/member/fileupload', formData);
-        console.log(result); // 응답 데이터 확인
-        console.log(result.data); // result.data가 실제로 존재하는지 확인
-        setImgSrc(`http://localhost:8070/userimg/${result.data.filename}`);
-        setImgStyle({display:"block", width:"200px"});
-        setProfileimg(result.data.filename)
+    async function fileUpload(e) {
+        const file = e.target.files[0];
+        if (file) {
+            const formData = new FormData();
+            formData.append('image', file);
+            const result = await axios.post('/api/member/fileupload', formData);
+            setImgSrc(`http://localhost:8070/userimg/${result.data.filename}`);
+            setImgStyle({display:'block', width:'200px'});
+            setProfileimg(result.data.filename);
+        } else {
+            setImgSrc('');
+            setImgStyle({display:'none'});
+            setProfileimg('');
+        }
     }
-
+    
     const uuid = () => {
         return ([1e7]+-1e3+-4e3+-8e3+-1e11).replace(/[018]/g, c =>
             (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
@@ -394,109 +422,111 @@ const Savekakaoinfo = () => {
     }, [identityVerificationId, code, message]);
 
     return (
-    <div>
-{isLoginSuccess?(!loadingComplete ? (
-                    <Loading onComplete={handleLoadingComplete} />
-                ) : null)
-:
-(<>
-<div className='join-container'>
-    {showModal && (
-        <div className="modal-overlay">
-            <div className="modal-content">
-                <p>로그인 중입니다...</p>
-            </div>
-        </div>
-    )}
-            <div className='login-btns'>
-                <div className="login-btn" onClick={ ()=>{   handleIdentityVerification()    }  }>성인인증</div>
-            </div>
-            <div className='joinform'>
-                <div className='field'>
-                    <label className="hidden">NAME</label>
-                    <input type="text" placeholder="이름" value={memberName} onChange={(e)=>{setMemberName(e.currentTarget.value)}} readOnly/>
+    <div className='kakao-container'>
+        {isLoginSuccess?(!loadingComplete ? (
+                            <Loading onComplete={handleLoadingComplete} />
+                        ) : null)
+        :
+        (<>
+            <div className='join-container' id='kakao-join'>
+                {showModal && (
+                    <div className="modal-overlay">
+                        <div className="modal-content">
+                            <p>로그인 중입니다...</p>
+                        </div>
+                    </div>
+                )}
+                <div className='login-btns'>
+                    <div className="login-btn" id='adultVerification' onClick={ ()=>{   handleIdentityVerification()    }  }>성인인증</div>
                 </div>
+                <div className='joinform'>
+                    <div className='field'>
+                        <label className='hidden'>NAME</label>
+                        <input type='text' placeholder='이름' id='name' value={memberName} onChange={(e)=>{setMemberName(e.currentTarget.value)}} readOnly/>
+                    </div>
 
-                <div className='field flex-row'>
-                    <div className='gender'>
-                    <label className="hidden">GENDER</label>
-                        {/* 인증시 받아오는 경우 */}
-                        <input type="text" placeholder="성별"
-                        value={gender === "" || gender === null || gender === undefined ? "" : gender === 0 ? "남성" : "여성"}
-                        readOnly
+                    <div className='field flex-row'>
+                        <div className='gender'>
+                        <label className='hidden'>GENDER</label>
+                            {/* 인증시 받아오는 경우 */}
+                            <input type='text' placeholder='성별'
+                            value={gender === "" || gender === null || gender === undefined ? "" : gender === 0 ? "남성" : "여성"}
+                            readOnly
+                        />
+                        </div>
+
+                        <div className='birth'>
+                        <label className='hidden'>BIRTHDATE</label>
+                            {/* 수정 불가능 하게 할 경우 */}
+                            <input
+                                type='text' // type을 date로 변경
+                                placeholder='생년월일'
+                                value={birthDate}
+                                required
+                        />
+                        </div>
+                    </div>
+
+                    <div className='field'>
+                        <label className="hidden">PHONE</label>
+                        <input type="text" placeholder="전화번호" value={phone} onChange={(e)=>{setPhone(e.currentTarget.value)}} readOnly/>
+                    </div>
+
+                    <div className='field'>
+                        <label className='hidden'>E-MAIL</label>
+                        <input type='text' id='email' name='email' autoComplete='email' placeholder='E-MAIL (로그인 시 아이디로 사용됩니다.)' value={email} onChange={(e)=>{setEmail(e.currentTarget.value)}}/>
+                    </div>
+                    <div className='field'>
+                        <label className="hidden">NICKNAME</label>
+                        <input type="text" id='nickname' placeholder="닉네임" value={nickname} onChange={(e)=>{setNickname(e.currentTarget.value)}}/>
+                    </div>
+                    <div className='field flex-row'>
+                        <div className='zipnum'>
+                            <label className="hidden">ZIPNUM</label>
+                            <input type="text" value={zipnum} readOnly placeholder="우편번호" />
+                        </div>
+                        <div className='login-btn' id='k-zipcode' onClick={() => setIsAddressModalOpen(true)}>
+                            <label>주소 검색</label>
+                            <button className='hidden'>주소 검색</button>
+                        </div>
+                    </div>
+                    <div className='field'>
+                        <label className="hidden">ADDRESS</label>
+                        <input type="text" value={address} readOnly placeholder="주소" />
+
+                    </div>
+                    <div className='field'>
+                        <label className="hidden">INTRO</label>
+                        <input type="text" placeholder="한마디" id='intro' value={intro} onChange={handleIntroChange} />
+                        <span className='profile-msg'>{intro.length} / 255</span>
+                    </div>
+                    <div className='field' id='profileimg'>
+                        <label className='hidden'>PROFILE IMG</label>
+                        <input type='file' name='profileimg' accept='.jpg,.jpeg,.png,.gif' onChange={(e)=>{fileUpload(e)}}/>
+                    </div>
+                    {imgSrc && (
+                        <div className='field' id='prev'>
+                            <label className='hidden'>Profile img preview</label>
+                            <div className='prevImg'><img src={imgSrc} style={imgStyle} alt="Profile preview" /></div>
+                        </div>
+                    )}
+
+                    <AddressModal
+                        isOpen={isAddressModalOpen}
+                        onClose={() => setIsAddressModalOpen(false)}
+                        onComplete={handleComplete}
                     />
-                    </div>
-
-                    <div className='birth'>
-                    <label className="hidden">BIRTHDATE</label>
-                        {/* 수정 불가능 하게 할 경우 */}
-                        <input
-                            type="text" // type을 date로 변경
-                            placeholder="생년월일"
-                            value={birthDate}
-                            required
-                    />
-                    </div>
                 </div>
 
-                <div className='field'>
-                    <label className="hidden">PHONE</label>
-                    <input type="text" placeholder="전화번호" value={phone} onChange={(e)=>{setPhone(e.currentTarget.value)}} readOnly/>
+                <div className='login-btns'>
+                    <div className="login-btn" onClick={ ()=>{   onSubmit()    }  }><IoCreateOutline />JOIN</div>
                 </div>
-
-                <div className='field'>
-                    <label className='hidden'>E-MAIL</label>
-                    <input type='text' name='email' autoComplete='email' placeholder='E-MAIL (로그인 시 아이디로 사용됩니다.)' value={email} onChange={(e)=>{setEmail(e.currentTarget.value)}}/>
-                </div>
-                <div className='field'>
-                    <label className="hidden">NICKNAME</label>
-                    <input type="text" placeholder="닉네임" value={nickname} onChange={(e)=>{setNickname(e.currentTarget.value)}}/>
-                </div>
-                <div className='field flex-row'>
-                    <div className='zipnum'>
-                        <label className="hidden">ZIPNUM</label>
-                        <input type="text" value={zipnum} readOnly placeholder="우편번호" />
-                    </div>
-                    <div className='login-btn' onClick={() => setIsAddressModalOpen(true)}>
-                        <label>주소 검색</label>
-                        <button className='hidden'>주소 검색</button>
-                    </div>
-                </div>
-                <div className='field'>
-                    <label className="hidden">ADDRESS</label>
-                    <input type="text" value={address} readOnly placeholder="주소" />
-
-                </div>
-                <div className='field'>
-                    <label className="hidden">INTRO</label>
-                    <input type="text" placeholder="한마디" value={intro} onChange={(e)=>{setIntro(e.currentTarget.value)}}/>
-                </div>
-                <div className='field'>
-                    <label className="hidden">PROFILE IMG</label>
-                    <input type="file" accept=".jpg,.jpeg,.png,.gif" onChange={(e)=>{fileUpload(e)}}/>
-                </div>
-                <div className='field'>
-                    <label className="hidden">Profile img preview</label>
-                    <div><img src={imgSrc} style={imgStyle} /></div>
-                </div>
-                <AddressModal
-                    isOpen={isAddressModalOpen}
-                    onClose={() => setIsAddressModalOpen(false)}
-                    onComplete={handleComplete}
-                />
             </div>
-
-            <div className='login-btns'>
-                <div className="login-btn" onClick={ ()=>{   onSubmit()    }  }><IoCreateOutline />JOIN</div>
-            </div>
-        </div>
-
-
 
 
         </>)}
 
-        </div>
+    </div>
 
     )
 }
